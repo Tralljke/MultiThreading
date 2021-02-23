0bd4c3557e39c6c97a373cd8c31e82e79bc87665
Petr_Belyaev <petr_bel96@icloud.com>
2021-02-23 23:47:29 +0700

    Sync by method

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+
+public class Main {
+
+    static int counter = 0;
+    static volatile boolean finish = false;
+    static volatile boolean finish1 = false;
+
+    public synchronized static void add() {
+        counter++;
+    }
+    public static void main(String[] args) throws InterruptedException {
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 200_000; i++) {
+                    add();
+                }
+                finish = true;
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 200_000; i++) {
+                    add();
+                }
+                finish1 = true;
+            }
+        }).start();
+
+        while (!finish || !finish1);
+
+        System.out.println(counter);
+
+    }
+
+
+}
+
+
