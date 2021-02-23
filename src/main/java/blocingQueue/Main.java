a85fd5b453c9912e12a66be22802ba108e66c99f
Petr_Belyaev <petr_bel96@icloud.com>
2021-02-24 00:37:39 +0700

    Test Apps

---
+++ src/main/java/blocingQueue/Main.java
@@ -0,0 +1,47 @@
+package blocingQueue;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ Метод main, где можно посмотреть, как работает BlockingQueue.
+ Несколько потоков кладут в очередь данные, Один поток их забирает.
+ **/
+
+public class Main {
+
+    public static void main(String[] args) {
+        final BlockingQueue<String> queue = new ArrayBlockingQueue<>(32);
+
+        for (int i = 0; i < 7; i++) {
+            final int threadNumber = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int counter = 0;
+                    while (true) {
+                        try {
+                            Thread.sleep(1000);
+                            queue.put("elem-" + threadNumber + "/" + ++counter);
+                            System.out.println(Thread.currentThread().getName() + " put: " + counter);
+                        } catch (InterruptedException ignore) { }
+                    }
+                }
+            }).start();
+        }
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            String data = queue.take();
+                            System.out.println(Thread.currentThread().getName() + " take: " + data);
+                        } catch (InterruptedException ignore) {
+                        }
+                    }
+                }
+            }).start();
+        }
+
+}
